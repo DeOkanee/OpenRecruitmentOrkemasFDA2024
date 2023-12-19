f7a980d1470da64357492defa8084bd5a47ae6cf
De Oka <115537848+DeOkanee@users.noreply.github.com>
2023-12-19 10:38:18 +0800

    update

--- js/info.js
+++ js/info.js
@@ -5,16 +5,33 @@ document.addEventListener('DOMContentLoaded', function () {
   const pendaftaranDate = '19-22 Des 2023';
   const wawancaraDate = '23-24 Des 2023';
 
+  // Cek apakah pop-up sudah ditampilkan sebelumnya
+  const isPopupShown = localStorage.getItem('popupShown');
+
+  // Menampilkan SweetAlert setelah 6 detik jika belum ditampilkan sebelumnya
+  if (!isPopupShown) {
+      setTimeout(function() {
+          // Menampilkan SweetAlert dengan info pendaftaran dan wawancara
+          Swal.fire({
+              icon: "info",
+              title: "Info",
+              html: `<p>1. Pendaftaran: ${pendaftaranDate}</p><p>2. Wawancara: ${wawancaraDate}</p>`,
+          });
+
+          // Menandai bahwa pop-up sudah ditampilkan
+          localStorage.setItem('popupShown', 'true');
+      }, 6000); // 6000 milidetik = 6 detik
+  }
+
   // Event listener untuk menampilkan pop-up saat Info diklik
   infoButton.addEventListener('click', function (event) {
-    event.preventDefault();
+      event.preventDefault();
 
-    // Menampilkan SweetAlert dengan info pendaftaran dan wawancara
-    Swal.fire({
-      icon: "info",
-      title: "Info",
-      text: "Pendaftaran dan Wawancara",
-      html: `<p>~ Pendaftaran: ${pendaftaranDate}</p><p>~ Wawancara: ${wawancaraDate}</p>`,
-    });
+      // Menampilkan SweetAlert dengan info pendaftaran dan wawancara
+      Swal.fire({
+          icon: "info",
+          title: "Info",
+          html: `<p>1. Pendaftaran: ${pendaftaranDate}</p><p>2. Wawancara: ${wawancaraDate}</p>`,
+      });
   });
 });
