94b25ac39a3f768f1f6a31efd24f64e13caadcc7
De Oka <115537848+DeOkanee@users.noreply.github.com>
2023-12-19 10:31:06 +0800

    update

--- js/info.js
+++ js/info.js
@@ -1,16 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const infoButton = document.getElementById('infoButton');
-  
-    // Event listener untuk menampilkan pop-up saat Info diklik
-    infoButton.addEventListener('click', function (event) {
-      event.preventDefault();
-  
-      // Menampilkan SweetAlert
-      Swal.fire({
-        icon: "info",
-        title: "Oops...",
-        text: "Belum ada info!!",
-      });
+  const infoButton = document.getElementById('infoButton');
+
+  // Daftar pendaftaran dan wawancara
+  const pendaftaranDate = '19-22 Des 2023';
+  const wawancaraDate = '23-24 Des 2023';
+
+  // Event listener untuk menampilkan pop-up saat Info diklik
+  infoButton.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // Menampilkan SweetAlert dengan info pendaftaran dan wawancara
+    Swal.fire({
+      icon: "info",
+      title: "Info",
+      text: "Pendaftaran dan Wawancara",
+      html: `<p>~ Pendaftaran: ${pendaftaranDate}</p><p>~ Wawancara: ${wawancaraDate}</p>`,
     });
   });
-  
+});
